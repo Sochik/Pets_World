6eefa37b3b8995065ea128dbce4656eb6d5b8c96
Sochik <petersorochi@gmail.com>
2025-11-19 09:54:09 -0800

    fix SectionHeader

--- src/Components/SectionHeader.jsx
+++ src/Components/SectionHeader.jsx
@@ -1,18 +1,21 @@
 import { title_img } from "../assets/Images";
 
-export default function SectionHeader(title, desc) {
-    return(
-        <div className="flex flex-col items-center gap-1 lg:gap-3 lg:py-3">
-            <h2
-              className="text-highlight font-extrabold text-2xl md:text-5xl font-calligraffitti"
-              data-aos="fade-down"
-            >
-              {title}
-            </h2>
-            <p className="text-primary text-base md:text-2xl font-Caveat">
-              {desc}
-            </p>
-            <img src={title_img} alt="" />
-          </div>
-    )
+export default function SectionHeader({ title, desc }) {
+  return (
+    <div className="flex flex-col items-center gap-1 lg:gap-3 lg:py-3">
+      <h2
+        className="text-highlight font-extrabold text-2xl md:text-5xl font-calligraffitti"
+        data-aos="fade-down"
+      >
+        {title}
+      </h2>
+      <p className="text-primary text-base md:text-2xl font-Caveat">
+        {desc}
+      </p>
+      <img
+        src={title_img}
+        alt={title ? `${title} decorative` : "section decorative image"}
+      />
+    </div>
+  );
 }
